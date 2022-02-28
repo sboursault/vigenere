be8fd396adac0afb8a8ea4f5eea997c4147ff059
sboursault <sboursault@protonmail.com>
2022-02-28 19:59:17 +0100

    cleaning

---
+++ progress.py
@@ -0,0 +1,23 @@
+import time
+import sys
+
+
+def update_progress(count, total):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('\r[%s] %s%s' % (bar, percents, '%'))
+    sys.stdout.flush()
+
+
+def fake_progress():
+    for i in range(101):
+        time.sleep(.07)
+        update_progress(i, 100)
+    time.sleep(1)
+
+
+fake_progress()
